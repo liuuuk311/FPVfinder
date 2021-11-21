42ee853a25ba916409b5e5c9f9f057aaec191d1d
Luca Palonca <luca.palonca@sailogy.com>
2021-11-21 17:03:00 +0100

    changes for localization

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,17 +1,38 @@
 <script context="module">
-  import '../i18n'
-  import { waitLocale } from 'svelte-i18n'
+  // import '../i18n'
+  // import { waitLocale } from 'svelte-i18n'
 
-  export async function preload() {
-    // awaits for the loading of the 'en-US' and 'en' dictionaries
-    return waitLocale()
-  }
+  // export async function preload() {
+  //   // awaits for the loading of the 'en-US' and 'en' dictionaries
+  //   return waitLocale()
+  // }
 </script>
 <script>
 	import '../assets/css/tailwind.css'
   import '../assets/css/app.css'
 
+  import {
+    register,
+    init,
+    getLocaleFromNavigator,
+    isLoading,
+  } from "svelte-i18n";
+
+  // note it's ending is .json, while the filename in static/ is  .json.js
+  register('en', () => import('../locales/en.json'));
+  register('it', () => import('../locales/it.json'));
+  // locales are not available yet
+
+  init({
+    fallbackLocale: 'en',
+    initialLocale: getLocaleFromNavigator(),
+  });
+
+
 </script>
 
-<slot />
-
+{#if $isLoading}
+  Loading...
+{:else}
+  <slot />
+{/if}
