525da9b1fb819974b860acb3a98fb4b972f48efe
Luca Palonca <luca.palonca@sailogy.com>
2021-12-01 14:17:37 +0100

    Added filter button

--- src/routes/search.svelte
+++ src/routes/search.svelte
@@ -74,20 +74,26 @@
 <p>Loading</p>
 {:then data}
 {#if data.count > 0}
-<div class="flex flex-row my-3 mx-2 justify-between align-middle">
-	<p class="text-md text-gray-600 md:text-lg">{$_('results_count')}: {data.count}</p>
-	<div class="w-2/6 max-w-xs float-left">
-		<div class="relative">
-		  <select class="appearance-none w-full bg-gray-200 text-gray-700 p-2 rounded">
-			<option>{$_('order_by_relevance')}</option>
-			<option>{$_('order_by_popularity')}</option>
-			<option>{$_('order_by_price')}</option>
-		  </select>
-		  <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-			<svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
-		  </div>
+<div class="flex flex-row my-3 mx-2 justify-between">
+	<p class="text-md text-gray-600 md:text-lg my-auto">{$_('results_count')}: {data.count}</p>
+	<div class="flex flex-row w-1/2 max-w-xs justify-end">
+		<div class="mx-2">
+			<button class="appearance-none w-full bg-gray-200 text-gray-700 p-2 rounded">Filtri</button>
 		</div>
-	  </div>
+		<div class="">
+			<div class="relative">
+			  <select class="appearance-none w-full bg-gray-200 text-gray-700 p-2 pr-8 rounded">
+				<option>{$_('order_by_relevance')}</option>
+				<option>{$_('order_by_popularity')}</option>
+				<option>{$_('order_by_price')}</option>
+			  </select>
+			  <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+				<svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+			  </div>
+			</div>
+		  </div>
+	</div>
+	
 </div>
 <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-1 lg:gap-2">
 	{#each data.results as product}
