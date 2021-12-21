10b247b2fec56208df50ca0531a8e7217067ebb1
Luca Palonca <luca.palonca@sailogy.com>
2021-12-21 07:47:14 +0100

    Suggestions reset on mount

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,10 @@
 <script>
 	import HomeSearchBar from '../compontents/home/HomeSearchBar.svelte';
 	import { suggestions } from "../stores/suggestionsStore";
-	import { onDestroy } from 'svelte';
+	import { onDestroy, onMount } from 'svelte';
 	
 	onDestroy(() => suggestions.set([]));
+	onMount(() => suggestions.set([]));
 </script>
 <svelte:head>
 	<title>FPV Finder</title>
